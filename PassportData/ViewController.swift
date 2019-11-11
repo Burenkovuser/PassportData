68630b1f96f6a82b632852dd3d289c8e15cc4a81
Burenkovuser <nb2207@yandex.ru>
2019-11-11 16:27:04 +0700

    Initial Commit

---
+++ PassportData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PassportData
+//
+//  Created by Vasilii on 11.11.2019.
+//  Copyright © 2019 Vasilii Burenkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
